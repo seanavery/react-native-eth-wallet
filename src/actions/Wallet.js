791d26015f9a9deab9e1e566311736f4a8fce1e3
Sean Pollock <seanpollock3344@gmail.com>
2017-03-31 20:32:18 -0500

    succesfully setting http provider for node'ified web3

--- src/actions/Wallet.js
+++ src/actions/Wallet.js
@@ -1,5 +1,6 @@
 import Promise from 'bluebird';
 // import Tx from 'ethereumjs-tx';
+import Web3 from 'web3';
 
 export default class Wallet {
   constructor() {
@@ -8,6 +9,15 @@ export default class Wallet {
 
   load() {
     console.log('hit load function')
+    return new Promise((resolve, reject) => {
+      Promise.delay(0)
+      .then(() => {
+        console.log('Web3', Web3);
+        let web3 = new Web3()
+        web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+        console.log('web3', web3);
+      });
+    });
   }
 
   web3() {
