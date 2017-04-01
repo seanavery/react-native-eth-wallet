90d24613e7214fb262ea9800591bbf6957d247a8
Sean Pollock <seanpollock3344@gmail.com>
2017-03-31 21:15:26 -0500

    Adding method to createVault from eth-lightwallet keystore.

--- src/actions/Wallet.js
+++ src/actions/Wallet.js
@@ -1,10 +1,11 @@
 import Promise from 'bluebird';
-// import Tx from 'ethereumjs-tx';
+import Tx from 'ethereumjs-tx';
+import { keystore, txUti}
 import Web3 from 'web3';
 
 export default class Wallet {
   constructor() {
-
+    this.password = '';
   }
 
   load() {
@@ -13,10 +14,27 @@ export default class Wallet {
       Promise.delay(0)
       .then(() => {
         console.log('Web3', Web3);
-        let web3 = new Web3()
+        let web3 = new Web3();
         web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
         console.log('web3', web3);
+      }).catch((error) => {
+        reject(error);
+      })
+    });
+  }
+
+  createAccount(password) {
+    console.log('keystore', keystore);
+    return new Promise((resolve, reject) => {
+      // first, set local global to password
+      keystore.createVault({
+        password: this.password
+      }).then((error, pwDerivedKey) => {
+        console.log('pwDerivedKey', pwDerivedKey);
+      }).catch((error) => {
+        reject(error);
       });
+      // then, save hashed password in
     });
   }
 
